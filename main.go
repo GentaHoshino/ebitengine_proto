aa98ecc72de60e12a5e6dee75e0c9e02c4433551
GentaHoshino <1180samuraijp@gmail.com>
2025-08-17 23:33:17 +0900

    画像を仮置きしよう

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 )
 
 type game struct {
-	bg     *ebiten.Image
-	person *ebiten.Image
+	bg        *ebiten.Image
+	person    *ebiten.Image
+	cat       *ebiten.Image
+	messageBG *ebiten.Image
 }
 
 func newGame() (*game, error) {
@@ -28,6 +30,17 @@ func newGame() (*game, error) {
 	}
 	g.person = img
 
+	img, _, err = ebitenutil.NewImageFromFile("cat.png")
+	if err != nil {
+		return nil, err
+	}
+	g.cat = img
+
+	img, _, err = ebitenutil.NewImageFromFile("message_haikei.png")
+	if err != nil {
+		return nil, err
+	}
+	g.messageBG = img
 	w, h := g.bg.Bounds().Dx(), g.bg.Bounds().Dy()
 	ebiten.SetWindowSize(w, h) // ← ウィンドウを画像サイズに
 	// もし拡大縮小なしの固定内部解像度にしたいなら：
@@ -46,7 +59,19 @@ func (g *game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(g.bg, op)
 
 	op = &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(-1, 1)
+	op.GeoM.Translate(1280, 0)
 	screen.DrawImage(g.person, op)
+
+	op = &ebiten.DrawImageOptions{}
+	// op.GeoM.Scale(0.5, 0.5)
+	// op.GeoM.Translate(560, 0)
+	screen.DrawImage(g.cat, op)
+
+	op = &ebiten.DrawImageOptions{}
+
+	op.GeoM.Translate(0, 450)
+	screen.DrawImage(g.messageBG, op)
 }
 
 func (g *game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
